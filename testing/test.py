6385c6a3f6de0628743e250916025ac6a3160d04
mykytabrykov <mykyta.brykov@gmail.com>
2021-01-16 10:42:13 +0100

    test1

--- testing/test.py
+++ testing/test.py
@@ -2,7 +2,7 @@ import json
 from types import SimpleNamespace
 import datetime
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, A
+from elasticsearch_dsl import Search, A, connections
 from elasticsearch_dsl import connections
 from es_query_template import EsQueryTemplate
 
@@ -22,37 +22,51 @@ query = {
         }
     }
 
+
 es_client = EsConfig().get_es_client()
 
-s = Search(using=es_client).index("exim*").filter("term", python__analyzed="false").exclude("terms", source__ip=["127.0.0.1","::1"]).query("exists", field="event.id")
+connections.add_connection(conn=es_client, alias="default")
+
+s = Search(index="exim*").filter("term", python__analyzed="false").exclude("terms", source__ip=["127.0.0.1","::1"]).query("exists", field="event.id")
 
 
-a = A('terms', field='event.id.keyword', size=25)
-s = s[0:15]
-s.aggs.bucket('event_id_terms', a)
+a = A('terms', )
+s.aggs.bucket('emails_per_day', 'date_histogram', field="event.timestamp", calendar_interval='day')\
+    .bucket('emails_per_hour', 'date_histogram', field="event.timestamp", calendar_interval='hour')\
+    .bucket('event_id_terms', "terms", field='event.id.keyword')
+
+print("Query preview: ", s.to_dict())
 
 resp = s.execute()
-print("resp: ", dir(resp))
-print("Dict: ", s.to_dict())
-print("aggs1", s.aggs['event_id_terms'])
 
-
-
-for hit in resp.aggregations.event_id_terms.buckets:
-    s1 = Search(using=es_client).index("exim*").filter("term", event__id__keyword=hit.key)
+for event_id_bucket in resp.aggregations.event_id_terms.buckets:
+    s1 = Search(using=es_client).index("exim*").filter("term", event__id__keyword=event_id_bucket.key)
     a1 = A('terms', field="event.action.keyword", size=5)
     s1.aggs.bucket('event_action_terms', a1)
-    s1 = s1[0:hit.doc_count]
     resp = s1.execute()
+    #print("s1:", s1.to_dict())
 
-    print("s1:", s1.to_dict())
-    for hit0 in resp.aggregations.event_action_terms.buckets:
-        print(hit0)
-    for hit1 in s1:
-        print("Ev.id", hit1.event.id, "event.action", hit1.event.action)
-    break
+    sent = False
+    failed = False
+    deferred = False
+    sent_event_count = 0
+    for event_action_bucket in resp.aggregations.event_action_terms.buckets:
+        if event_action_bucket.key == 'email-sent':
+            sent = True
+            sent_event_count = event_action_bucket.doc_count
+        elif event_action_bucket.key == 'email-deferred':
+            deferred = True
+            deferred_event_count = event_action_bucket.doc_count
+        elif event_action_bucket.key == 'email-failed':
+            failed = True
+            failed_event_count = event_action_bucket.doc_count
 
-for hit in resp:
-    print("event.id", hit)
+
+
+    print("Event ID: ", event_id_bucket.key,"Sent:", sent,"(",sent_event_count,")", " - Failed: ", failed, " - Deferred: ", deferred)
+
+
+#for event_id_bucket in resp:
+    #print("event.id", event_id_bucket)
 
 
